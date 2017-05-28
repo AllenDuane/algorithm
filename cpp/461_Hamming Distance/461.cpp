4329c9b247464bb9f231ce6b69bcc9ed4becd4a5
SimonDuan <simonduan@yeah.net>
2017-05-28 15:40:09 +0800

    update 461

---
+++ cpp/461_Hamming Distance/461.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int res = 0;
+        while(x!=0||y!=0){
+            if ((x&0x1) != (y&0x1))
+                res++;
+            x = x>>1;
+            y = y>>1;
+        }
+        return res;
+    }
+};
